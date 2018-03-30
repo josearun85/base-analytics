a4bc2a194cd36258dc396cdd20d4b9ef6e49f779
josearun85 <josearun85@gmail.com>
2018-03-30 08:59:26 +0530

    Add files via upload

---
+++ cross_validation.R
@@ -0,0 +1,38 @@
+library(caret)
+setwd("D://base-analytics-master/data")
+house <- read.csv("house_prices.csv")
+house$Home<-NULL
+
+split<-createDataPartition(y = house$Price, p = 0.6, list = FALSE)
+
+train<-house[split,]
+
+test<-house[-split,]
+
+fit <- train(Price ~ .,train,method = "lm", metric="Rsquared")
+summary(fit)
+fit
+
+# Cross validation bootstrap
+cvctrl<-trainControl(method = "cv" , repeats = 10)
+fit <- train(Price ~ .,house,method = "lm", trControl = cvctrl,  metric="Rsquared")
+fit
+#plot(fit)
+summary(fit)
+
+# Cross validation cv+number = kfold
+kctrl<-trainControl(method = "cv",number = 10)
+fit <- train(Price ~ .,house,method = "lm", trControl = kctrl,  metric="Rsquared")
+fit
+
+rpkctrl<-trainControl(method = "repeatedcv",number = 10, repeats= 4)
+fit <- train(Price ~ .,house,method = "lm", trControl = rpkctrl,  metric="Rsquared")
+fit
+
+# Cross validation jackknifing
+jkctrl<-trainControl(method = "LOOCV")
+fit <- train(Price ~ .,house,method = "lm", trControl = jkctrl,  metric="Rsquared")
+fit
+summary(fit)
+getModelInfo("lm")
+library(randomForest)
